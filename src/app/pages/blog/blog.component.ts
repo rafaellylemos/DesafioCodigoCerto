ab89adb6d95b2e424a143ae682deedd4fbea3b2e
Rafaelly Lemos <rafaellylemos@hotmail.com>
2024-09-19 10:23:12 -0300

    feat(blog): adiciona responsividade mobile

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { PostComponent } from './post/post.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [PostComponent, PersonalInfoComponent],
+  imports: [PostComponent, PersonalInfoComponent, CommonModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss',
 })
-export class BlogComponent {}
+export class BlogComponent {
+ 
+}
--- src/app/pages/blog/post/post.component.ts
+++ src/app/pages/blog/post/post.component.ts
@@ -1,17 +1,57 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss',
 })
-export class PostComponent {
+export class PostComponent implements AfterViewInit{
   @Input() img01: string = '';
   @Input() img02: string = '';
   @Input() img03: string = '';
   @Input() img04: string = '';
   @Input() img05: string = '';
   @Input() img06: string = '';
+
+  @ViewChild('textContent') textContent!: ElementRef<HTMLDivElement>;
+
+  isExpanded = false;
+  initialVisibleLines = 15;
+  linesPerClick = 7;
+
+  ngAfterViewInit() {
+    this.updateVisibleLines();
+  }
+
+  updateVisibleLines() {
+    if (this.textContent) {
+      const content = this.textContent.nativeElement;
+      const lineHeight = parseFloat(getComputedStyle(content).lineHeight);
+    
+      content.style.maxHeight = `${this.initialVisibleLines * lineHeight}px`;
+    }
+  }
+
+  toggleText() {
+    const content = this.textContent.nativeElement;
+    const lineHeight = parseFloat(getComputedStyle(content).lineHeight);
+    const contentHeight = content.scrollHeight;
+  
+    if (this.isExpanded) {
+      this.initialVisibleLines = 15;
+      this.isExpanded = false;
+    } else {
+      this.initialVisibleLines += this.linesPerClick;
+      
+      if (this.initialVisibleLines * lineHeight >= contentHeight) {
+        this.initialVisibleLines = Math.floor(contentHeight / lineHeight);
+        this.isExpanded = true; 
+      }
+    }
+    
+    this.updateVisibleLines();
+  }
 }
